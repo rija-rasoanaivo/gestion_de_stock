96801a15ee7da00dd4c9812f1f24f518e3c9f9fe
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2024-02-01 14:56:52 +0100

    Push du code Product après test

--- Product.py
+++ Product.py
@@ -5,4 +5,26 @@ class Product:
         self.table =  'product'
         self.database = Database(host='localhost', user='root', password='Rija', database='store')
 
+    def create(self, name, description, price, quantity, id_category):
+        query = f"INSERT INTO {self.table} (name, description, price, quantity, id_category) VALUES (%s, %s, %s, %s, %s)"
+        parametre = (name, description, price, quantity, id_category)
+        self.database.executeQuery(query, parametre)
+
+    def read(self):
+        query = f"SELECT * FROM {self.table}"
+        return self.database.fetch(query)
     
+    def update(self, id, name, description, price, quantity, id_category):
+        query = f"UPDATE {self.table} SET name=%s, description=%s, price=%s, quantity=%s, id_category=%s WHERE id=%s"
+        parametre = (name, description, price, quantity, id_category, id)
+        self.database.executeQuery(query, parametre)
+
+    def delete(self, id):
+        query = f"DELETE * FROM {self.table} WHERE id=%s"
+        parametre = (id,)
+        self.database.executeQuery(query, parametre)
+
+    def find(self, id):
+        query = f"SELECT * FROM {self.table} WHERE id=%s"
+        paramatre = (id,)
+        return self.database.fetch(query, paramatre)
---
+++ main.py
@@ -0,0 +1,8 @@
+from Product import Product
+
+product_manager = Product()
+
+# product_manager.create("Viande grillé", "Veau et Agneau", 8, 2, 3)
+# print(product_manager.read())
+# product_manager.update(5, 'Viande pour Kebab','Veau agneau et boeuf', 7, 2, 3)
+# print(product_manager.find(2))
